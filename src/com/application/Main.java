7961717d2b9fd450340e90d7f740da049935f4fe
Lucas Bardelli <lucas.bardelli@hotmail.com>
2022-04-25 14:28:41 -0300

    Class 'Position' created

---
+++ src/com/application/Main.java
@@ -0,0 +1,12 @@
+package com.application;
+
+import boardgame.Position;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+    }
+}
